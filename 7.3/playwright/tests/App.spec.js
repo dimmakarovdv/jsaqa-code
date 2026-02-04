14478fda777559a60d54c284b7acb7b6cd7b6c1c
Neto test <dimmakarovdv89@mail.ru>
2026-02-04 21:40:43 +0500

    I have finalized the tests for the second task

--- 7.3/playwright/tests/App.spec.js
+++ 7.3/playwright/tests/App.spec.js
@@ -3,38 +3,51 @@ const userData = require('../user');
 
 test.describe('Авторизация на netology.ru', () => {
   test('Валидный вход', async ({ page }) => {
-    await page.goto('https://netology.ru');
+    await page.goto('https://netology.ru/?al=sign_in');
+    await expect(page).toHaveURL('https://netology.ru/?al=sign_in');
 
-    await page.locator('a:has-text("Войти")').click();
-    await page.getByText('Войти по почте', { exact: true }).click();
+    await page
+      .locator('a')
+      .filter({ hasText: 'Войти' }, { timeout: 30000 })
+      .first()
+      .click();
 
-    await page.type('input[name="email"]', userData.validEmail);
-    await page.type('input[name="password"]', userData.validPassword);
+    await page.locator(':text-is("Войти по почте")').click();
+    await page.locator('[placeholder="Email"]').click();
+    await page.locator('[placeholder="Email"]').fill(userData.validEmail);
 
-    await page.locator('button').filter({ hasText: 'Войти' }).first().click();
+    await page.locator('[placeholder="Пароль"]').click();
+    await page.locator('[placeholder="Пароль"]').fill(userData.validPassword);
 
-    await expect(page.getByText('Здравствуйте', { exact: true })).toBeVisible({
-      timeout: 120000,
-    });
+    await page.locator('[data-testid="login-submit-btn"]').click();
+
+    await page.waitForURL(`https://netology.ru/profile/${userData.userID}`);
+    await expect(page).toHaveURL(
+      `https://netology.ru/profile/${userData.userID}`
+    );
   });
 
   test('Попытка входа с неверным логином и паролем', async ({ page }) => {
-    await page.goto('https://netology.ru');
-    await page.locator('a:has-text("Войти")').click();
+    await page.goto('https://netology.ru/?al=sign_in');
+    await expect(page).toHaveURL('https://netology.ru/?al=sign_in');
 
-    await expect(
-      page.getByRole('link', { name: 'Войти по почте' })
-    ).toBeVisible();
-    await page.getByRole('link', { name: 'Войти по почте' }).click();
+    await page
+      .locator('a')
+      .filter({ hasText: 'Войти' }, { timeout: 30000 })
+      .first()
+      .click();
 
-    await page.locator('input[type="email"]').fill(userData.invalidEmail);
-    await page.locator('input[type="password"]').fill(userData.invalidPassword);
-    await page.locator('button:has-text("Войти")').click();
+    await page.locator(':text-is("Войти по почте")').click();
 
-    await page.waitForTimeout(1000);
+    await page.locator('[placeholder="Email"]').click();
+    await page.locator('[placeholder="Email"]').fill(userData.invalidEmail);
 
-    await expect(
-      page.locator('.notification, .error, [role="alert"]')
-    ).toHaveText(/неверный|Неверный/, { timeout: 5000 });
+    await page.locator('[placeholder="Пароль"]').click();
+    await page.locator('[placeholder="Пароль"]').fill(userData.invalidPassword);
+
+    await page.locator('[data-testid="login-submit-btn"]').click();
+
+    const error = await page.locator('[data-testid="login-error-hint"]');
+    await expect(error).toHaveText('Вы ввели неправильно логин или пароль');
   });
 });
